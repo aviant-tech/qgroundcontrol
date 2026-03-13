516ba97783f88b3b918aaf7b53d96375102a233c
Torjus Bakkene <torjus.bakkene@gmail.com>
2026-03-13 17:42:36 +0100

    feat: receive aviant ats status as a fact

--- src/Vehicle/VehicleAviantFactGroup.cc
+++ src/Vehicle/VehicleAviantFactGroup.cc
@@ -1,5 +1,6 @@
 #include "VehicleAviantFactGroup.h"
 #include "Vehicle.h"
+#include "mavlink.h"
 
 #include <QtMath>
 
@@ -11,13 +12,16 @@ const QColor VehicleAviantFactGroup::COLOR_CRITICAL { QColor::fromRgb(255, 0, 0)
 VehicleAviantFactGroup::VehicleAviantFactGroup(QObject* parent)
     : FactGroup     (100, ":/json/Vehicle/AviantFactGroup.json", parent),
     _navigationAccuracyFact(0, navigationAccuracyFactName, FactMetaData::valueTypeUint8),
-    _navigationRedundancyFact(0, navigationRedundancyFactName, FactMetaData::valueTypeUint8)
+    _navigationRedundancyFact(0, navigationRedundancyFactName, FactMetaData::valueTypeUint8),
+    _atsStatusFact(0, atsStatusFactName, FactMetaData::valueTypeUint32)
 {
 
     _addFact(&_navigationAccuracyFact, navigationAccuracyFactName);
     _addFact(&_navigationRedundancyFact, navigationRedundancyFactName);
+    _addFact(&_atsStatusFact, atsStatusFactName);
     _navigationAccuracyFact.setRawValue(qQNaN());
     _navigationRedundancyFact.setRawValue(qQNaN());
+    _atsStatusFact.setRawValue(0);
 }
 
 void VehicleAviantFactGroup::handleMessage(Vehicle* vehicle, mavlink_message_t& message)
@@ -25,6 +29,8 @@ void VehicleAviantFactGroup::handleMessage(Vehicle* vehicle, mavlink_message_t& 
     switch (message.msgid) {
         case MAVLINK_MSG_ID_AVIANT_NAV:
             return handleNavMsg(vehicle, message);
+        case MAVLINK_MSG_ID_AVIANT_ATS_STATUS:
+            return handleAtsStatusMsg(vehicle, message);
         default:
             return;
     }
@@ -72,4 +78,45 @@ void VehicleAviantFactGroup::handleNavMsg(Vehicle* vehicle, mavlink_message_t& m
             break;
     }
     _navigationRedundancyFact.setRawValue(nav.redundancy);
+}
+
+void VehicleAviantFactGroup::handleAtsStatusMsg(Vehicle* vehicle, mavlink_message_t& message)
+{
+    mavlink_aviant_ats_status_t atsStatus;
+    mavlink_msg_aviant_ats_status_decode(&message, &atsStatus);
+
+    // These should match atsStatus in AviantFactGroup.json
+    constexpr uint32_t VALUE_BLANK = 0;
+    constexpr uint32_t VALUE_OK = 1;
+    constexpr uint32_t VALUE_WARN = 2;
+    constexpr uint32_t VALUE_FAIL = 3;
+
+    if (!atsStatus.ats_enabled) {
+        _atsStatusFact.setOverrideColor(COLOR_UNKNOWN);
+        _atsStatusFact.setRawValue(VALUE_BLANK);
+        return;
+    }
+    uint32_t ats_status_flags = atsStatus.ats_status_flags;
+    if (!atsStatus.power_loss_trigger_enabled) {
+        ats_status_flags &= ~AVIANT_ATS_STATUS_FLAG_POWER_LOSS;
+        ats_status_flags &= ~AVIANT_ATS_STATUS_FLAG_UPS_UNHEALTHY;
+    }
+    const bool ats_status_ok = ats_status_flags == 0;
+
+    const time_t now = time(nullptr);
+
+    if (ats_status_ok) {
+        _time_last_ats_status_ok = now;
+    }
+
+    if (ats_status_ok) {
+        _atsStatusFact.setOverrideColor(COLOR_NOMINAL);
+        _atsStatusFact.setRawValue(VALUE_OK);
+    } else if (now - _time_last_ats_status_ok > ATS_STATUS_HYSTERESIS_S) {
+        _atsStatusFact.setOverrideColor(COLOR_CRITICAL);
+        _atsStatusFact.setRawValue(VALUE_FAIL);
+    } else {
+        _atsStatusFact.setOverrideColor(COLOR_WARNING);
+        _atsStatusFact.setRawValue(VALUE_WARN);
+    }
 }
--- src/Vehicle/VehicleAviantFactGroup.h
+++ src/Vehicle/VehicleAviantFactGroup.h
@@ -4,6 +4,7 @@
 #include "QGCMAVLink.h"
 
 #include <QColor>
+#include <ctime>
 
 class VehicleAviantFactGroup : public FactGroup
 {
@@ -13,25 +14,33 @@ public:
     VehicleAviantFactGroup(QObject* parent = nullptr);
     Q_PROPERTY(Fact* navigationAccuracy    READ navigationAccuracy    CONSTANT)
     Q_PROPERTY(Fact* navigationRedundancy  READ navigationRedundancy  CONSTANT)
+    Q_PROPERTY(Fact* atsStatus             READ atsStatus             CONSTANT)
 
     Fact* navigationAccuracy() { return &_navigationAccuracyFact; }
     Fact* navigationRedundancy() { return &_navigationRedundancyFact; }
+    Fact* atsStatus() { return &_atsStatusFact; }
     // Overrides from FactGroup
     void handleMessage(Vehicle* vehicle, mavlink_message_t& message) override;
 
 private:
     void handleNavMsg(Vehicle* vehicle, mavlink_message_t& message);
+    void handleAtsStatusMsg(Vehicle* vehicle, mavlink_message_t& message);
 
     static void setDefaultColorOverride(Fact& fact, int state);
 
     Fact _navigationAccuracyFact;
     Fact _navigationRedundancyFact;
+    Fact _atsStatusFact;
 
     static constexpr const char* navigationAccuracyFactName = "navigationAccuracy";
     static constexpr const char* navigationRedundancyFactName = "navigationRedundancy";
+    static constexpr const char* atsStatusFactName = "atsStatus";
 
     static const QColor COLOR_UNKNOWN;
     static const QColor COLOR_NOMINAL;
     static const QColor COLOR_WARNING;
     static const QColor COLOR_CRITICAL;
+
+    static constexpr uint32_t ATS_STATUS_HYSTERESIS_S = 1;
+    time_t _time_last_ats_status_ok = 0;
 };
